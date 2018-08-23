831d75b81cf3ea7fb913cee93a0a2dea1755fcd8
Zak Thompson <zak.dev.th@gmail.com>
2018-08-22 22:59:00 -0400

    Basic dev setup

---
+++ client/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.less';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Campaign Forge</h1>
+      </div>
+    );
+  }
+}
